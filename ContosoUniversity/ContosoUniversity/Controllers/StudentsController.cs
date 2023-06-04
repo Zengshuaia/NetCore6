2828331280d5ac0134740b3995a65358aa14675e
zeng <2741903696@qq.com>
2023-06-04 22:34:47 +0800

    看完数据迁移部分

--- ContosoUniversity/ContosoUniversity/Controllers/StudentsController.cs
+++ ContosoUniversity/ContosoUniversity/Controllers/StudentsController.cs
@@ -20,9 +20,44 @@ namespace ContosoUniversity.Controllers
         }
 
         // GET: Students
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string sortOrder,string currentFilter,string searchString,int? pageNumber)//排序功能
         {
-            return View(await _context.Students.ToListAsync());
+            ViewData["CurrentSort"] = sortOrder;
+            ViewData["NameSortParm"] = string.IsNullOrEmpty(sortOrder) ? "name_desc" : "";//初始化
+            ViewData["DateSortParm"] = sortOrder == "Date" ? "date_desc" : "Date";
+            if (searchString != null)
+            {
+                pageNumber = 1;
+            }
+            else
+            {
+                searchString = currentFilter; ;
+            }
+            ViewData["CurrentFilter"] = searchString;
+            var students = from s in _context.Students
+                           select s;
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                students = students.Where(s => s.LastName.Contains(searchString) || s.FirstMidName.Contains(searchString));//查询功能
+            }
+            switch (sortOrder)//第一次没有参数所以默认按照姓排序
+            {
+                case "name_desc":
+                    students = students.OrderByDescending(e => e.LastName);
+                    break;
+                case "Date":
+                    students = students.OrderBy(e => e.EnrollmentDate);
+                    break;
+                case "date_desc":
+                    students = students.OrderByDescending(e => e.EnrollmentDate);
+                    break;
+                default:
+                    students = students.OrderBy(e => e.LastName);
+                    break;
+            }
+            //return View(await students.AsNoTracking().ToListAsync());
+            int pageSize = 3;
+            return View(await PaginatedList<Student>.CreateAsync(students.AsNoTracking(), pageNumber ?? 1, pageSize));
         }
 
         // GET: Students/Details/5
